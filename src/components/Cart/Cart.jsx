0cb84f00b14d033d8adf46fe2d73fe7aab4189d1
Igor Kolpakov <gragass00@yandex.ru>
2022-10-28 12:13:53 +0300

    finish cart page

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -5,13 +5,14 @@ import { Link } from 'react-router-dom'
 import CartItem from '../CartItem/CartItem'
 import { ReactComponent as Trash } from '../../assets/img/trashIcon.svg'
 import { ReactComponent as CartIcon } from '../../assets/img/cart.svg'
+import { ReactComponent as BackIcon } from '../../assets/img/back.svg'
 
 const Cart = () => {
   return (
     <>
       <div className={styles.cartTop}>
         <div className={styles.titleWrapper}>
-          <CartIcon/>
+          <CartIcon />
           <h2>Корзина</h2>
         </div>
         <div className={styles.trash}>
@@ -22,6 +23,21 @@ const Cart = () => {
       <ul>
         <li><CartItem /></li>
       </ul>
+      <div className={styles.cartBottom}>
+        <div className={styles.valueWrapper}>
+          <span>Всего пицц: <b>5 шт.</b></span>
+          <span>Сумма заказа: <b className={styles.price}>1000 &#8381;</b> </span>
+        </div>
+        <div className={styles.buttonsWrapper}>
+          <div className={styles.buttonWrapper}>
+            <Link to='/'>
+              <BackIcon />
+              <span>Вернуться назад</span>
+            </Link>
+          </div>
+          <div className={styles.payButton}><span>Оплатить сейчас</span></div>
+        </div>
+      </div>
       <div className={styles.wrapper}>
         <h1>Корзина пустая</h1>
         <p>Вероятней всего, вы не заказывали ещё пиццу.
@@ -33,8 +49,6 @@ const Cart = () => {
         </Link>
       </div>
     </>
-
-
   )
 }
 
