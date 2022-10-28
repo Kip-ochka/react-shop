f0ad673f8d877aadec3737f35dfdb40720741509
Igor Kolpakov <gragass00@yandex.ru>
2022-10-28 10:20:26 +0300

    added cart item component

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,21 +1,26 @@
 import React from 'react'
 import styles from './Cart.module.scss'
 import cartSvg from '../../assets/img/emptycart.svg'
-import remove from '../../assets/img/icons8-trash-can.svg'
 import { Link } from 'react-router-dom'
 import CartItem from '../CartItem/CartItem'
+import { ReactComponent as Trash } from '../../assets/img/trashIcon.svg'
+import { ReactComponent as CartIcon } from '../../assets/img/cart.svg'
 
 const Cart = () => {
   return (
     <>
       <div className={styles.cartTop}>
+        <div className={styles.titleWrapper}>
+          <CartIcon/>
           <h2>Корзина</h2>
+        </div>
         <div className={styles.trash}>
+          <Trash />
           <span>Очистить корзину</span>
         </div>
       </div>
       <ul>
-        <li><CartItem/></li>
+        <li><CartItem /></li>
       </ul>
       <div className={styles.wrapper}>
         <h1>Корзина пустая</h1>
--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -1,28 +1,32 @@
 import React from 'react'
 import style from './CartItem.module.scss'
-import {ReactComponent as Minus} from '../../assets/img/minus.svg'
-import {ReactComponent as Plus} from '../../assets/img/plus.svg'
+import { ReactComponent as Minus } from '../../assets/img/minus.svg'
+import { ReactComponent as Plus } from '../../assets/img/plus.svg'
 
 const CartItem = () => {
   return (
     <div className={style.wrapper}>
       <div className={style.label}>
         <img
-          src='https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/d2e337e9-e07a-4199-9cc1-501cc44cb8f8.jpg' />
+          src='https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/d2e337e9-e07a-4199-9cc1-501cc44cb8f8.jpg' alt='Товар'/>
         <div className={style.typography}>
           <h3>Товар</h3>
           <p>описание, товара</p>
         </div>
       </div>
-      <div>
-        <button className={style.minus}>
+      <div className={style.valueWrapper}>
+        <div className={style.minus}>
           <Minus />
-        </button>
-        <b>Кол-во</b>
-        <button className={style.plus}><Plus/></button>
+        </div>
+        <b>100</b>
+        <div className={style.plus}>
+          <Plus />
+        </div>
       </div>
-      <b>цена</b>
-      <button className={style.delete}><Plus/></button>
+      <b>1200<span>&#8381;</span></b>
+      <div className={style.delete}>
+        <Plus />
+      </div>
     </div>
   )
 }
